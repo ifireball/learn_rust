e1eae6d235f1ed06c86c71c87e119140d41e4c8f
Barak Korren <barak.korren@gmail.com>
2022-10-01 11:27:24 +0300

    mingrep with iterators

--- src/bin/minigrep.rs
+++ src/bin/minigrep.rs
@@ -5,14 +5,11 @@ use learn_rust::minigrep::Config;
 use learn_rust::minigrep;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(
-        |err| {
+    let config = Config::build(env::args())
+        .unwrap_or_else(|err| {
             eprintln!("Can't read config: {err}");
             process::exit(1);
-        }
-    );
+        });
 
     if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {e}");
--- src/minigrep.rs
+++ src/minigrep.rs
@@ -24,30 +24,22 @@ pub fn run(config: Config)
 pub fn search<'a>(
     query: &str, contents: &'a str
 ) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
     query: &str, contents: &'a str
 ) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line)
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| {
+            line.to_lowercase().contains(&query)
+        })
+        .collect()
 }
 
 pub struct Config {
@@ -57,19 +49,26 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) 
-        -> Result<Self, &'static str> 
-    {
-        if args.len() < 3 {
-            Err("Not enough arguments!")
-        } else {
-            Ok(Self{
-                query: args[1].clone(),
-                file_path: args[2].clone(),
-                ignore_case: 
-                    env::var("IGNORE_CASE").is_ok(),
-            })
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>,
+    ) -> Result<Self, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(v) => v,
+            None => return Err("No query str"),
+        };
+        let file_path = match args.next() {
+            Some(v) => v,
+            None => return Err("No file name"),
+        };
+
+        Ok(Self{
+            query,
+            file_path,
+            ignore_case: 
+                env::var("IGNORE_CASE").is_ok(),
+        })
     }
 }
 
